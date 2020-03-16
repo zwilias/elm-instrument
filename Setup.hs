4272db2aea742c8b54509e536fa4f35d04f95da5
Marek Fajkus <marek.faj@gmail.com>
2020-03-16 19:46:43 +0100

    cabal >= 1.25 and ghc 8.8 support
    resolves #3

--- Setup.hs
+++ Setup.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE CPP #-}
 import Distribution.Simple
+#if MIN_VERSION_Cabal(1,25,0)
+import Distribution.Simple.BuildPaths (autogenComponentModulesDir)
+#else
 import Distribution.Simple.BuildPaths (autogenModulesDir)
+#endif
 import Distribution.Simple.LocalBuildInfo (LocalBuildInfo)
 import Distribution.Simple.Setup (BuildFlags)
 import Distribution.PackageDescription (PackageDescription, emptyHookedBuildInfo)
+import Distribution.Simple.LocalBuildInfo (withLibLBI)
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath ((</>))
 import System.Process (readProcess)
@@ -10,13 +16,19 @@ import System.Process (readProcess)
 
 main = defaultMainWithHooks $ simpleUserHooks { buildHook = myBuildHook }
 
-
 myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 myBuildHook packageDescription buildInfo userHooks buildFlags =
-    do
-        createDirectoryIfMissing True (autogenModulesDir buildInfo)
-        writeCustomFile (autogenModulesDir buildInfo </> "Build_elm_instrument.hs")
-        buildHook simpleUserHooks packageDescription buildInfo userHooks buildFlags
+#if MIN_VERSION_Cabal(1,25,0)
+  withLibLBI packageDescription buildInfo $ \_ libCfg -> do
+    createDirectoryIfMissing True (autogenComponentModulesDir buildInfo libCfg)
+    writeCustomFile (autogenComponentModulesDir buildInfo libCfg </> "Build_elm_instrument.hs")
+    buildHook simpleUserHooks packageDescription buildInfo userHooks buildFlags
+#else
+  do
+    createDirectoryIfMissing True (autogenModulesDir buildInfo)
+    writeCustomFile (autogenModulesDir buildInfo </> "Build_elm_instrument.hs")
+    buildHook simpleUserHooks packageDescription buildInfo userHooks buildFlags
+#endif
 
 
 writeCustomFile :: FilePath -> IO ()
